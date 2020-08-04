854fbe209f89466a74f9f25b50ec33a48a4eda4d
MatheStine <cmturner072@gmail.com>
2020-08-04 17:00:22 -0500

    quotes

--- popular _data_types.py
+++ popular _data_types.py
@@ -8,3 +8,29 @@ print(receipt)
 
 
 # Your total is 120.0
+
+
+
+#To change variable values in python
+
+first = 'Christine'
+second = 'Brodie'
+third = 'Abe'
+
+print(first)
+
+first = third
+print(first)
+
+
+
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+sentence_two = 'That is my dog\'s bowl'
+
+sentence_three = "That is my dog\'s bowl"
+
+sentence_four = "Tiffany said, \"That is my dog's bowl\""
+print(sentence_four)
+
+#backslash to hide needed aprostrashe
